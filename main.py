308cd8f87838eee72e866a74d749ac028aa7843b
TheSpace-hub <78466469+TheSpace-hub@users.noreply.github.com>
2025-08-30 14:53:58 +0300

    Возможность купить

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from typing import Any
-from math import floor
+from math import ceil
 
 import json
 
@@ -7,7 +7,7 @@ import yaml
 
 
 def get_building_blocks() -> dict[str, dict[str, Any]]:
-    with open('building_blocks.json', 'r', encoding='utf-8') as f:
+    with open('blocks.json', 'r', encoding='utf-8') as f:
         return json.loads(f.read())
 
 
@@ -24,15 +24,29 @@ def main():
 
     for material, value in get_building_blocks().items():
         name: str = value['name']
-        price: int = value['price']
+        price: int = value['price'] * 16
         menu['items'][material] = {
             'material': material,
+            'amount': 16,
             'display_name': f'&b{name}',
             'slot': slot,
             'lore': [
                 f'&7Купить ЛКМ за {price}₽',
-                f'&7Продать ПКМ за {floor(price / 2)}₽'
-            ]
+                f'&7Продать ПКМ за {ceil(price / 2)}₽'
+            ],
+            'left_click_commands': [
+                f'[console] give %player_name% {material} 16',
+                f'[takemoney] {price}'
+            ],
+            'left_click_requirement': {
+                'requirements': {
+                    'has_money': {
+                        'type': 'has money',
+                        'amount': price
+                    }
+                },
+                'deny_commands': ['[message]&b| &cИди и заработай деньги!']
+            }
         }
 
         slot += 1
@@ -42,7 +56,7 @@ def main():
             slot = 28
         elif slot == 35:
             with open(f'shop_building_blocks_{menu_index}.yml', 'w', encoding='utf-8') as file:
-                file.write(yaml.dump(menu))
+                file.write(yaml.dump(menu, allow_unicode=True))
             menu_index += 1
             slot = 10
             menu: dict = {
@@ -52,8 +66,7 @@ def main():
                 'items': {}
             }
     with open(f'shop_building_blocks_{menu_index}.yml', 'w', encoding='utf-8') as file:
-        file.write(yaml.dump(menu))
-
+        file.write(yaml.dump_all(menu, allow_unicode=True))
 
 
 if __name__ == '__main__':
