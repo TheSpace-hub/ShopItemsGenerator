ef377d34d8052a5f71c138423bb35102e5381c56
TheSpace-hub <78466469+TheSpace-hub@users.noreply.github.com>
2025-08-30 16:53:17 +0300

    Добавил название папки

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ from menu_builder import MenuBuilder
 def main():
     MenuBuilder('Строительные блоки. %page% // %pages%',
                 'building_blocks',
-                'shop_building_blocks_%page%').generate()
+                'shop_building_blocks%page%',
+                'shop_building_blocks').generate()
 
 
 if __name__ == '__main__':
