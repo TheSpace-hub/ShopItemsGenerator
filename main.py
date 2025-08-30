fea9d763b4d3cdf8244ddd9766cdf57d4c93b977
TheSpace-hub <78466469+TheSpace-hub@users.noreply.github.com>
2025-08-30 16:57:21 +0300

    Очистка папки output

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
+import shutil
+import os
+
 from menu_builder import MenuBuilder
 
 
+def recreate_output():
+    """
+    Очищает папку 'output'.
+    :return:
+    """
+    if os.path.exists('output'):
+        shutil.rmtree('output')
+    os.mkdir('output')
+
+
 def main():
+    recreate_output()
     MenuBuilder('Строительные блоки. %page% // %pages%',
                 'building_blocks',
                 'shop_building_blocks%page%',
